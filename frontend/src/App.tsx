ad3e86836e8030ef087a1d90dde1edc491ff4aec
nameIsAniket <aniketlakde7262@gmail.com>
2024-09-15 08:55:19 +0530

    adding publish page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { SignUp } from './pages/Signup'
 import { SignIn } from './pages/Signin'
 import { Blog } from './pages/Blog'
 import { Blogs } from './pages/Blogs.tsx'
+import { Publish } from './pages/Publish.tsx'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path = '/signin' element = {<SignIn/>} />
         <Route path = '/blog/:id' element = {<Blog/>} />
         <Route path = '/blogs' element = {<Blogs/>} />
+        <Route path = '/publish' element = {<Publish/>} />
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,10 +1,17 @@
 import { Avatar } from "./BlogCard"
+import { Link } from "react-router-dom"
 
 export const Appbar = () => {
-    return <div className="border-b border-slate-300 flex justify-between px-10 py-3">
-        <div className="flex justify-center">
+    return <div className="border-b border-slate-300 flex justify-between px-10 py-2">
+        <div className="flex justify-center flex-col">
              Medium 
         </div>
-        <div> <Avatar name = "Aniket" size={"big"}></Avatar> </div>
+        
+        <div className="flex items-center"> 
+            <Link to={`/publish`} className="flex items-center">
+                <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2 text-center">New Blog</button>
+            </Link>
+            <Avatar name = "Aniket" size={"big"}></Avatar>
+        </div>
     </div>
 }
