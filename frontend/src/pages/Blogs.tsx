d9d3d252c72b7d4419e0d43049d76ae89e7f314c
nameIsAniket <aniketlakde7262@gmail.com>
2024-09-15 12:24:32 +0530

    added BlogSkeleton

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,17 +1,28 @@
 import {BlogCard} from './../components/BlogCard'
 import {Appbar} from './../components/Appbar'
 import {useBlogs} from './../hooks/index'
+import {BlogSkeleton} from './../components/BlogSkeleton'
 
 export const Blogs = () => {
 
     const {loading, blogs} = useBlogs();
 
     if (loading){
-        return <div className='justify justify-center flex-col'>
-            ... Loading
+        return <div>
+            <div>
+                <Appbar/>
+            </div>
+            <div>
+                <BlogSkeleton/>
+                <BlogSkeleton/>
+                <BlogSkeleton/>
+                <BlogSkeleton/>
+                <BlogSkeleton/>
+                <BlogSkeleton/>
+            </div>
         </div>
     }
-    
+
     return <div>
         <div>
             <Appbar/>
